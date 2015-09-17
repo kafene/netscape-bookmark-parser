075882222c1f57b00ebf8ce2c4e7f7519ba9d19e
kafene <kafene@users.noreply.github.com>
2015-09-17 18:04:09 -0400

    Remove comment block; now in README

--- parse_netscape_bookmarks.php
+++ parse_netscape_bookmarks.php
@@ -3,26 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/**
- * Basically netscape bookmark files often come so badly formed, there's
- * no reliable way I could find to parse them with DOM or SimpleXML,
- * even after running HTML Tidy on them. So, this function does a bunch of
- * transformations on the general format of a netscape bookmark file, to get
- * Each bookmark and its description onto one line, and goes through line by
- * line, matching tags and attributes. It's messy, but it works better than
- * anything I could find in hours of googling, and anything that I could
- * write after hours with DOM and SimpleXML. I didn't want to pull in a big
- * DOM parsing library just to do this one thing, so this is it.
- * @todo - running Tidy before doing this might be beneficial.
- *   ?? $bkmk_str = tidy_parse_string($bkmk_str)->cleanRepair();
- *
- * Example:
- *     var_dump(parse_netscape_bookmarks(file_get_contents('bookmarks_export.htm')));
- * 
- * Update 2013-07-08:
- *     Just tested this on an export of some bookmarks from Pinboard.in
- *     and it seems that it is still working, so good for me.
- */
 function parse_netscape_bookmarks($bkmk_str, $default_tag = null) {
     $i = 0;
     $next = false;
