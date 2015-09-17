7c1eb4aaf3a5c3e3a43a62523a1ffd6180564924
kafene <kafene@users.noreply.github.com>
2015-09-17 18:11:21 -0400

    Update parse_netscape_bookmarks.php
    Remove MPL header

--- parse_netscape_bookmarks.php
+++ parse_netscape_bookmarks.php
@@ -1,7 +1,4 @@
 <?php
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 function parse_netscape_bookmarks($bkmk_str, $default_tag = null) {
     $i = 0;
