e77f85b0662dbddd4ef46b7f9b151031897af1fb
VirtualTam <virtualtam@flibidi.net>
2016-02-25 23:04:23 +0100

    parsing: fix ADD_DATE matching & add Unix timestamp support
    Fixes #8
    
    Fixes:
    - [parsing] use the proper match for ADD_DATE
    - [parsing] do not attempt to convert Unix timestamps
    
    Modifications:
    - [parsing] move date parsing to a dedicated function
    
    Additions:
    - [tests] add dates to the Netscape input file
    - [tests] add coverage for date parsing
    
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- tests/ParseNetscapeBookmarksTest.php
+++ tests/ParseNetscapeBookmarksTest.php
@@ -21,6 +21,7 @@ class ParseNetscapeBookmarksTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Secret stuff', $bkm[0]['title']);
         $this->assertEquals(0, $bkm[0]['pub']);
         $this->assertEquals('private secret', $bkm[0]['tags']);
+        $this->assertEquals('971175336', $bkm[0]['time']);
         $this->assertEquals(
             'Super-secret stuff you\'re not supposed to know about',
             $bkm[0]['note']
@@ -29,6 +30,45 @@ class ParseNetscapeBookmarksTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('Public stuff', $bkm[1]['title']);
         $this->assertEquals(1, $bkm[1]['pub']);
         $this->assertEquals('public hello world', $bkm[1]['tags']);
+        $this->assertEquals('1456433748', $bkm[1]['time']);
         $this->assertEquals('', $bkm[1]['note']);
+    }
+
+    /**
+     * Parse log dates
+     */
+    public function test_parse_log_dates()
+    {
+        $this->assertEquals(
+            '971211336',
+            parse_bookmark_date('10/Oct/2000:13:55:36 -0700')
+        );
+        $this->assertEquals(
+            '971186136',
+            parse_bookmark_date('10/Oct/2000:13:55:36 +0000')
+        );
+        $this->assertEquals(
+            '971175336',
+            parse_bookmark_date('10/Oct/2000:13:55:36 +0300')
+        );
+    }
+
+    /**
+     * Parse Unix timestamps
+     */
+    public function test_parse_unix_dates()
+    {
+        $this->assertEquals(
+            '1456433748',
+            parse_bookmark_date('1456433748')
+        );
+        $this->assertEquals(
+            '971175336',
+            parse_bookmark_date('971175336')
+        );
+        $this->assertEquals(
+            '-371211336',
+            parse_bookmark_date('-371211336')
+        );
     }
 }
